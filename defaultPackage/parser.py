24f6af0f815cfd2941b12d22d3318852a16d6c59
Suleman Khalid <suleman.khalid15@gmail.com>
2019-09-10 16:12:03 +0500

    Added main method Added comments for improved readability

--- defaultPackage/parser.py
+++ defaultPackage/parser.py
@@ -1,12 +1,12 @@
+import os
+import re
 import sys
+import timeit
+from io import StringIO
 
 import bs4
 import nltk
-import os
-import re
-import timeit
 from bs4 import BeautifulSoup
-from io import StringIO
 from nltk.stem.snowball import SnowballStemmer
 
 path = "C:\\Users\\luky\\PycharmProjects\\Indexer\\corpus"
@@ -20,7 +20,7 @@ sumTimes = {}
 forwardIndexes = []
 
 
-def sumTime(key, value):
+def add_timing(key, value):
     global sumTimes
     if key not in sumTimes:
         sumTimes[key] = value
@@ -28,7 +28,7 @@ def sumTime(key, value):
         sumTimes[key] += value
 
 
-def tag_visible(element):
+def filter_tags(element):
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]', 'http:', 'a']:
         return False
     elif isinstance(element, bs4.element.Comment):
@@ -44,11 +44,11 @@ def tag_visible(element):
     return True
 
 
-def text_from_html(body):
+def apply_regex(body):
     soup = BeautifulSoup(body, 'html.parser')
     # print(soup.original_encoding)
     texts = soup.findAll(text=True)
-    visible_texts = filter(tag_visible, texts)
+    visible_texts = filter(filter_tags, texts)
     str = u" ".join(t.strip() for t in visible_texts)
     # print(str)
     regex = re.compile('[^a-zA-Z\d ]')
@@ -58,144 +58,152 @@ def text_from_html(body):
     # return str
 
 
-with open(stopListPath, "r") as file:
-    stop = file.read()
-    stopList = stop.split()
+def main():
+    global wordID
 
-for root, dirs, files in os.walk(path):
-    for file in files:
-        fileNames.append(file)
-        list.append(os.path.join(root, file))
-        # if file.endswith(".txt"):
-        #     #print(os.path.join(root, file))
-        #     list.append(os.path.join(root, file))
+    # read stoplist
+    with open(stopListPath, "r") as file:
+        stop = file.read()
+        stopList = stop.split()
 
-# nltk.download('punkt')
+    # read all file paths
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            fileNames.append(file)
+            list.append(os.path.join(root, file))
+            # if file.endswith(".txt"):
+            #     #print(os.path.join(root, file))
+            #     list.append(os.path.join(root, file))
 
-# open("doc_index.txt", "w").close()
+    # nltk.download('punkt')
+    # open("doc_index.txt", "w").close()
 
-startTime = timeit.default_timer()
+    # initialize variables
+    startTime = timeit.default_timer()
+    file_str = StringIO()
+    startIndex = 0
+    endIndex = len(list)
+    sumSpeeds = 0
 
-file_str = StringIO()
+    # for all files
+    for i in range(startIndex, endIndex):
 
-startIndex = 0
-endIndex = len(list)
+        # rawdata = open(list[i], 'rb').read()
+        # result = chardet.detect(rawdata)
+        # encoding = result['encoding']
+        # print(encoding)
 
-sumSpeeds = 0
+        loopStartTime = timeit.default_timer()
 
-for i in range(startIndex, endIndex):
+        file = open(list[i], errors='ignore')
 
-    # rawdata = open(list[i], 'rb').read()
-    # result = chardet.detect(rawdata)
-    # encoding = result['encoding']
-    # print(encoding)
+        time = timeit.default_timer()
 
-    loopStartTime = timeit.default_timer()
+        # parsing
+        result = apply_regex(file.read())
+        add_timing('parsing', timeit.default_timer() - time)
 
-    file = open(list[i], errors='ignore')
+        time = timeit.default_timer()
 
-    time = timeit.default_timer()
+        # tokenizing
+        wordsDup = nltk.wordpunct_tokenize(result)
+        add_timing('tokenizing', timeit.default_timer() - time)
 
-    # parsing
-    result = text_from_html(file.read())
-    sumTime('parsing', timeit.default_timer() - time)
+        time = timeit.default_timer()
 
-    time = timeit.default_timer()
+        # remove stop words, lowercase, and keep distinct
+        words = []
+        for word in wordsDup:
+            w = word.lower()
+            if w not in stopList:
+                newWord = stemmer.stem(w)
+                words.append(newWord)
+                if newWord not in wordList:
+                    wordList[newWord] = wordID
+                    wordID += 1
 
-    # tokenizing
-    wordsDup = nltk.wordpunct_tokenize(result)
-    sumTime('tokenizing', timeit.default_timer() - time)
+        # words = [stemmer.stem(word.lower()) for word in words if word.lower() not in stopList]
+        add_timing('stem&lower', timeit.default_timer() - time)
 
-    # remove stop words, lowercase, and distinct
-    time = timeit.default_timer()
+        # stemming
+        # words = [stemmer.stem(word) for word in words]
 
-    words = []
-    for word in wordsDup:
-        w = word.lower()
-        if w not in stopList:
-            newWord = stemmer.stem(w)
-            words.append(newWord)
-            if newWord not in wordList:
-                wordList[newWord] = wordID
-                wordID += 1
+        # check if word exists in dictionary, if not add it.
+        # time = timeit.default_timer()
+        #
+        # for word in words:
+        #     if word not in wordList:
+        #         wordList[word] = wordID
+        #         wordID += 1
+        # checkexists('addwordtolist', timeit.default_timer() - time)
+        #
+        time = timeit.default_timer()
 
-    # words = [stemmer.stem(word.lower()) for word in words if word.lower() not in stopList]
-    sumTime('stem&lower', timeit.default_timer() - time)
+        # write forward index
+        # with open("doc_index.txt", "a+", encoding="utf8") as file:
+        #     prevWords = []
+        #     for word in words:
+        #         if word not in prevWords:
+        #             prevWords.append(word)
+        #             indices = [str(i + 1) for i, x in enumerate(words) if x == word]
+        #             file.write(str(i) + "\t" + str(wordList[word]) + "\t" + "\t".join(indices) + "\n")
 
-    # stemming
-    # words = [stemmer.stem(word) for word in words]
+        # write forward index in ram
+        prevWords = {}
+        for word in words:
+            if word not in prevWords:
+                prevWords[word] = 0
+                indices = [str(i + 1) for i, x in enumerate(words) if x == word]
+                file_str.write(str(i) + "\t" + str(wordList[word]) + "\t" + "\t".join(indices) + "\n")
+                # forwardIndexes.append(str(i) + "\t" + str(wordList[word]) + "\t" + "\t".join(indices) + '\n')
 
-    # check if word exists in dictionary, if not add it.
-    # time = timeit.default_timer()
-    #
-    # for word in words:
-    #     if word not in wordList:
-    #         wordList[word] = wordID
-    #         wordID += 1
-    # checkexists('addwordtolist', timeit.default_timer() - time)
-    #
+        add_timing('writingtoStringIO', timeit.default_timer() - time)
+
+        # calculate speed
+        currSpeed = timeit.default_timer() - loopStartTime
+        sumSpeeds += currSpeed
+        if i % 10 == 0:
+            print('\rElapsed Time : ' + str(round(timeit.default_timer() - startTime, 1)), end='')
+            print(' Progress : ' + str(i) + '/' + str(endIndex - 1), end='')
+            print(' Remaining Time : ' + str(round((endIndex - 1 - i) * (sumSpeeds / (i + 1)), 0)), end='')
+            sys.stdout.flush()
+
+        file.close()
+
     time = timeit.default_timer()
 
     # write forward index
-    # with open("doc_index.txt", "a+", encoding="utf8") as file:
-    #     prevWords = []
-    #     for word in words:
-    #         if word not in prevWords:
-    #             prevWords.append(word)
-    #             indices = [str(i + 1) for i, x in enumerate(words) if x == word]
-    #             file.write(str(i) + "\t" + str(wordList[word]) + "\t" + "\t".join(indices) + "\n")
+    with open("doc_index.txt", "w", encoding="utf8") as file:
+        file.write(file_str.getvalue())
+        # file.write(''.join(forwardIndexes))
 
-    # write forward index in ram
-    prevWords = {}
-    for word in words:
-        if word not in prevWords:
-            prevWords[word] = 0
-            indices = [str(i + 1) for i, x in enumerate(words) if x == word]
-            file_str.write(str(i) + "\t" + str(wordList[word]) + "\t" + "\t".join(indices) + "\n")
-            # forwardIndexes.append(str(i) + "\t" + str(wordList[word]) + "\t" + "\t".join(indices) + '\n')
+    # write doc ids
+    with open("docids.txt", "w", encoding="utf8") as file:
+        index = 0
+        for name in fileNames:
+            file.write(str(index) + "\t" + name + "\n")
+            index += 1
 
-    sumTime('writingtoStringIO', timeit.default_timer() - time)
+    exceptions = 0
+    # write term ids
+    with open("termids.txt", "w", encoding='utf8') as file:
+        for key, value in wordList.items():
+            try:
+                file.write(str(value) + "\t" + key + "\n")
+            except UnicodeEncodeError:
+                exceptions += 1
+                continue
+                # print('Error printing key : ' + key.encode('utf-16', 'surrogatepass').decode('utf-16') + ' value : ' + str(value))
 
-    currSpeed = timeit.default_timer() - loopStartTime
-    sumSpeeds += currSpeed
-    if i % 10 == 0:
-        print('\rElapsed Time : ' + str(round(timeit.default_timer() - startTime, 1)), end='')
-        print(' Progress : ' + str(i) + '/' + str(endIndex - 1), end='')
-        print(' Remaining Time : ' + str(round((endIndex - 1 - i) * (sumSpeeds / (i + 1)), 0)), end='')
-        sys.stdout.flush()
+    add_timing('writingtofiles', timeit.default_timer() - time)
 
-    file.close()
+    stop = timeit.default_timer()
+    print('\n\nExceptions : ' + str(exceptions))
+    print('Time: ' + str(round(stop - startTime, 4)))
 
-time = timeit.default_timer()
+    for key in sumTimes:
+        print(key + ' : ' + str(round(sumTimes[key], 3)))
 
-# write forward index
-with open("doc_index.txt", "w", encoding="utf8") as file:
-    file.write(file_str.getvalue())
-    # file.write(''.join(forwardIndexes))
 
-# write doc ids
-with open("docids.txt", "w", encoding="utf8") as file:
-    index = 0
-    for name in fileNames:
-        file.write(str(index) + "\t" + name + "\n")
-        index += 1
-
-exceptions = 0
-# write term ids
-with open("termids.txt", "w", encoding='utf8') as file:
-    for key, value in wordList.items():
-        try:
-            file.write(str(value) + "\t" + key + "\n")
-        except UnicodeEncodeError:
-            exceptions += 1
-            continue
-            # print('Error printing key : ' + key.encode('utf-16', 'surrogatepass').decode('utf-16') + ' value : ' + str(value))
-
-sumTime('writingtofiles', timeit.default_timer() - time)
-
-stop = timeit.default_timer()
-print('\n\nExceptions : ' + str(exceptions))
-print('Time: ' + str(round(stop - startTime, 4)))
-
-for key in sumTimes:
-    print(key + ' : ' + str(round(sumTimes[key], 3)))
+if __name__ == "__main__":
+    main()
